8d65ffd92ade5f2680cffe47a01e354f0401f3f2
akubek <artur.kubek.96@gmail.com>
2026-04-24 16:41:55 +0200

    refactor: rename dependencies.php to services.php

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/../bootstrap/init.php';
 
 try {
     //prepare structures, container with data that is injected, and routes to map pages to actions
-    $container = require_once BASE_PATH . '/bootstrap/dependencies.php';
+    $container = require_once BASE_PATH . '/bootstrap/services.php';
     $routes = require_once BASE_PATH . '/config/routes.php';
-    
+
     $page = $_GET['page'] ?? 'home';
 
     // check if page exitsts
@@ -19,12 +19,11 @@ try {
     require BASE_PATH . '/views/partials/header.php';
     $routes[$page]($container); //todo move to renderView, and move renderView to actual controller logic (how to display)
     require BASE_PATH . '/views/partials/footer.php';
-
 } catch (Throwable $e) { // thorwable to catch everything
     error_log("CRITICAL ERROR: " . $e->getMessage() . " in " . $e->getFile() . ":" . $e->getLine());
-    
+
     http_response_code(500);
-     
+
     if (file_exists(BASE_PATH . '/views/errors/500.php')) {
         require BASE_PATH . '/views/errors/500.php';
     } else { // fallback to default 500 message
@@ -50,5 +49,3 @@ try {
         </html>';
     }
 }
-
-?>
